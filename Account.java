577f4a453d926999acbd1e3864dea030835e175d
Mannuru Sowjanya <167868221+Sowjanya2527@users.noreply.github.com>
2024-06-19 10:24:00 +0530

    Update

--- Account.java
+++ Account.java
@@ -3,7 +3,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Account {
-	// variables
+	
 	private int customerNumber;
 	private int pinNumber;
 	private double checkingBalance = 0;
